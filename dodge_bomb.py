6ebc46b6ba70ff821dee42ede0649ecc42510bc5
yusuke <c0a25116ae@edu.teu.ac.jp>
2026-06-16 14:24:30 +0900

    練習２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -7,8 +8,8 @@ WIDTH, HEIGHT = 1100, 650
 DELTA={
             pg.K_UP:(0,-5),
             pg.K_DOWN:(0,+5),
-            pg.K_RIGHT:(-5,0),
-            pg.K_LEFT:(+5,0)
+            pg.K_RIGHT:(+5,0),
+            pg.K_LEFT:(-5,0)
         }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -22,6 +23,15 @@ def main():
     kk_rct.center = 300, 200
     clock = pg.time.Clock()
     tmr = 0
+
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)
+    bb_img.set_colorkey((0,0,0))
+    bb_rct=bb_img.get_rect()
+    bb_rct.centerx = random.randint(0,WIDTH)
+    bb_rct.centery = random.randint(0,HEIGHT)
+    vx, vy  = +5, +5
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -47,6 +57,8 @@ def main():
 
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)
+        screen.blit(bb_img,bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
